7cec4faea4814db28892365d094c211c4025782b
Rex <hearnr16@asmsa.org>
2015-10-15 17:15:49 -0500

    yo

---
+++ Spoopy Ghost/Assets/Scripts/enemyController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyController : MonoBehaviour {
+	
+	private bool alerted = false;
+	private Rigidbody2D rb;
+	private bool movingLeft = false;
+	private bool movingRight = false;
+	public float speed = 5.0F;
+	public Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (alerted) {
+			if (player.transform.position.x > rb.transform.position.x) {
+				if (!movingLeft) {
+					rb.velocity.Set (speed, 0.0F);
+					movingRight = false;
+					movingLeft = true;
+				}
+			} else {
+				if (!movingRight) {
+					rb.velocity.Set (speed, 0.0F);
+					movingRight = true;
+					movingLeft = false;
+				}
+			}
+		} else {
+			movingLeft = false;
+			movingRight = false;
+			rb.velocity.Set (0F,0F);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "player") {
+			alerted = true;
+		}
+	}
+}
---
+++ Spoopy Ghost/Assets/Scripts/fallingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fallingPlatform : MonoBehaviour {
+
+	public Rigidbody2D player;
+	public float fallTime = 1.0F;
+	private float stepTime = -1.0F;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (stepTime != -1.0F) {
+			if (fallTime + stepTime >= Time.time) {
+				rb.gravityScale.Equals(2.0F);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "player") {
+			if (stepTime != -1.0F) {
+				stepTime = Time.time;
+			}
+		}
+	}
+}

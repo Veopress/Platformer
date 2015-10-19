101721c06eb1d5e32f88fdcee7a5fad7e82db7cc
Ray <murphyr16@asmsa.org>
2015-10-18 19:29:19 -0500

    cranberry

--- Spoopy Ghost/Assets/SpoopyGo.cs
+++ Spoopy Ghost/Assets/SpoopyGo.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class SpoopyGo : MonoBehaviour {
 
 	Rigidbody2D rb;
-	public bool leftKey= false, rightKey = false, upKey = false, isSecondJump = false, firstJump = true, phaseKey = false;
+	Animator anim;
+	public bool leftKey= false, rightKey = false, upKey = false, isSecondJump = false, firstJump = true, phaseKey = false, facingRight = false;
 	public float forceMagnitude = 13f, maxVelocity = 10f;
 	private int timer = 0;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
 	}
 
 	void OnTriggerEnter2D(Collider2D c){
@@ -29,6 +31,23 @@ public class SpoopyGo : MonoBehaviour {
 
 	void phase(){
 		rb.gravityScale = 0.5f;
+	}
+
+	void FixedUpdate(){
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		if (move > 0 && facingRight)
+			Flip ();
+		else if (move < 0 && !facingRight)
+			Flip ();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 	
 	// Update is called once per frame
@@ -64,7 +83,7 @@ public class SpoopyGo : MonoBehaviour {
 		
 		if (upKey && timer <= 0 && !isSecondJump) {
 			rb.AddRelativeForce (new Vector2 (0, forceMagnitude * 30));
-			timer = 50;
+			timer = 40;
 			if(!firstJump){
 				isSecondJump = true;
 			}else{
